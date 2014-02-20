82d9b962167d8994a35af60f276d906fce9b18f1
Chris Hoffman <cehoffman@gmail.com>
2014-02-19 22:04:36 -0600

    Cleanup avr-binutils a bit

--- avr-binutils.rb
+++ avr-binutils.rb
@@ -4,13 +4,12 @@ class AvrBinutils < Formula
   url 'http://ftpmirror.gnu.org/binutils/binutils-2.24.tar.bz2'
   mirror 'http://ftp.gnu.org/gun/binutils/binutils-2.24.tar.bz2'
   homepage 'http://www.gnu.org/software/binutils/binutils.html'
-  # sha1 '587fca86f6c85949576f4536a90a3c76ffc1a3e1'
+  sha1 '7ac75404ddb3c4910c7594b51ddfc76d4693debb'
 
-  option 'disable-libbfd', 'Disable installation of libbfd.'
+  option 'without-libbfd', 'Disable installation of libbfd.'
 
   def install
     ENV['CPPFLAGS'] = "-I#{include}"
-    p ENV['CPPFLAGS']
 
     args = ["--prefix=#{prefix}",
             "--infodir=#{info}",
@@ -23,7 +22,7 @@ class AvrBinutils < Formula
       system './configure', '--enable-install-libbfd', *args
       system 'make'
       system 'make', 'install'
-    end unless build.include? 'disable-libbfd'
+    end unless build.without? 'libbfd'
 
     # brew's build environment is in our way
     ENV.delete 'CFLAGS'
