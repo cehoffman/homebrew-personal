abd1ab2d6f0d11a10d4f228239604f58776dc665
Chris Hoffman <cehoffman@gmail.com>
2014-02-21 21:53:45 -0600

    Working and updated avr tools

--- avr-binutils.rb
+++ avr-binutils.rb
@@ -9,15 +9,16 @@ class AvrBinutils < Formula
   option 'without-libbfd', 'Disable installation of libbfd.'
 
   def install
-    ENV['CPPFLAGS'] = "-I#{include}"
+    args = [
+      "--prefix=#{prefix}",
+      "--infodir=#{info}",
+      "--mandir=#{man}",
+      '--disable-werror',
+      '--disable-nls',
+      '--with-dwarf2',
+    ]
 
-    args = ["--prefix=#{prefix}",
-            "--infodir=#{info}",
-            "--mandir=#{man}",
-            '--disable-werror',
-            '--disable-nls']
-
-    Dir.chdir 'bfd' do
+    cd 'bfd' do
       ohai 'building libbfd'
       system './configure', '--enable-install-libbfd', *args
       system 'make'
@@ -31,9 +32,14 @@ class AvrBinutils < Formula
   end
 
   def patches
-    # Support for -C in avr-size. See issue 
-    # https://github.com/larsimmisch/homebrew-avr/issues/9
-    { :p0 => 'https://gist.github.com/larsimmisch/4190960/raw/b36f3d6d086980006f097ae0acc80b3ada7bb7b1/avr-binutils-size.patch' }
+    {
+      :p0 => ['https://gist.github.com/larsimmisch/4190960/raw/b36f3d6d086980006f097ae0acc80b3ada7bb7b1/avr-binutils-size.patch'],
+      :p1 => [
+              # 'https://gist.githubusercontent.com/cehoffman/9110871/raw/dd29a43ec96dfb43d128afa2c53a9b00902cfd94/binutils-avr-coff.patch',
+              'https://gist.githubusercontent.com/cehoffman/9110871/raw/dd0bda58fa1bc21b5604a0b9399b647b6c50d261/binutils-as-dwarf.patch',
+              'https://gist.githubusercontent.com/cehoffman/9110871/raw/7e51e3daf97c5e3bbe4ab7138808ea16672aed69/binutils-dwarf2-AVRStudio-workaround.patch',
+      ],
+    }
   end
 
 end
--- avr-gcc.rb
+++ avr-gcc.rb
@@ -13,12 +13,30 @@ class AvrGcc < Formula
   depends_on 'libmpc'
   depends_on 'mpfr'
 
-  option 'with-cxx', 'build with support for C++'
+  option 'without-cxx', 'build without support for C++'
+
+  resource 'avr-libc' do
+    url 'http://download.savannah.gnu.org/releases/avr-libc/avr-libc-1.8.0.tar.bz2'
+    sha1 '2e3815221be8e22f5f2c07b922ce92ecfa85bade'
+  end
+
+  resource 'avr-libc-manpages' do
+    url 'http://download.savannah.gnu.org/releases/avr-libc/avr-libc-manpages-1.8.0.tar.bz2'
+    sha1 '586cf60bb9d2b67498b73b38b546f7b4620dc86c'
+  end
+
+  resource 'avr-libc-html' do
+    url 'http://download.savannah.gnu.org/releases/avr-libc/avr-libc-user-manual-1.8.0.tar.bz2'
+    sha1 '54f991e63c46eb430986bea3bae0e28cbe0b87c8'
+  end
 
   def install
     binutils = Formula.factory('avr-binutils')
     args = [
             "--target=avr",
+            '--enable-long-long',
+            '--disable-checking',
+            '--disable-shared',
             "--disable-libssp",
             "--disable-nls",
             "--with-dwarf2",
@@ -27,20 +45,14 @@ class AvrGcc < Formula
             "--with-gmp=#{Formula.factory('gmp').opt_prefix}",
             "--with-mpfr=#{Formula.factory('mpfr').opt_prefix}",
             "--with-mpc=#{Formula.factory('libmpc').opt_prefix}",
-            # ...except the stuff in share...
-            "--datarootdir=#{share}",
-            # ...and the binaries...
-            "--bindir=#{bin}",
-            "--with-sysroot=#{Formula.factory('avr-libc').opt_prefix}/avr",
-            '--with-build-sysroot',
             # This shouldn't be necessary
-            "--with-as=#{binutils.bin}/avr-as",
-            "--with-ld=#{binutils.bin}/avr-ld"
+            "--with-as=#{binutils.opt_prefix}/bin/avr-as",
+            "--with-ld=#{binutils.opt_prefix}/bin/avr-ld"
            ]
 
     # The C compiler is always built, C++ can be disabled
     languages = %w[c]
-    languages << 'c++' if build.with? 'cxx'
+    languages << 'c++' if build.without? 'cxx'
 
     mkdir 'build' do
       system '../configure', "--enable-languages=#{languages.join(',')}", *args
@@ -50,12 +62,25 @@ class AvrGcc < Formula
       # deja-gnu and autogen formulae must be installed in order to do this.
 
       system 'make', 'install'
-
-      multios = %x[gcc --print-multi-os-directory].strip
-
-      # binutils already has a libiberty.a. We remove ours, because
-      # otherwise, the symlinking of the keg fails
-      File.unlink "#{prefix}/lib/#{multios}/libiberty.a"
     end
+
+    resource('avr-libc').stage do
+      ENV['CC'] = bin/'avr-gcc'
+      ENV['CXX'] = bin/'avr-g++'
+      system "./configure",
+        "--build=#{%x[./config.guess].strip}",
+        "--prefix=#{prefix}",
+        "--host=avr"
+      system 'make', 'install'
+    end
+
+    man.install resource('avr-libc-manpages')
+    (share/'doc/avr-libc').install resource('avr-libc-html')
+  end
+
+  def caveats; <<-EOS.undent
+    Include the following directory in your path to use avr-gcc easily.
+      #{opt_prefix}/bin
+    EOS
   end
 end
