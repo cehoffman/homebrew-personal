7ddfe269f61ba3eea02c67876987a4fb16bdd346
Chris Hoffman <cehoffman@gmail.com>
2014-03-17 11:02:53 -0500

    Convert vim patch to block type

--- vim.rb
+++ vim.rb
@@ -34,8 +34,9 @@ class Vim < Formula
   conflicts_with 'ex-vi',
     :because => 'vim and ex-vi both install bin/ex and bin/view'
 
-  def patches
-    [ 'https://12985092515833507022.googlegroups.com/attach/b72719c27b178f52/pum-silent.diff?part=2&view=1&vt=ANaJVrFwPjohkoxKQsd4PhRnvyCOouxHwe5LGAcwweas-n_oakomC7AKXTiPFqpGSOPTvsX57-s4bw-YKanaOJV72RQCTruZ_zOX1bjM6VRQk_CCUSZCj7E' ]
+  patch do
+    url 'https://12985092515833507022.googlegroups.com/attach/b72719c27b178f52/pum-silent.diff?part=2&view=1&vt=ANaJVrFwPjohkoxKQsd4PhRnvyCOouxHwe5LGAcwweas-n_oakomC7AKXTiPFqpGSOPTvsX57-s4bw-YKanaOJV72RQCTruZ_zOX1bjM6VRQk_CCUSZCj7E'
+    sha1 '3a159b9d24076d27647eea4aeb9d637b7af7a00f'
   end
 
   def install
