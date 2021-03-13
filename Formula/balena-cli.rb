1466cf3b6a8ed9563ce55cac0ccd00fb20b9af22
Chris Hoffman <cehoffman@gmail.com>
2021-03-13 00:33:49 -0600

    Drop old forumla and add lua-language-server

--- Formula/balena-cli.rb
+++ Formula/balena-cli.rb
@@ -1,11 +1,10 @@
 class BalenaCli < Formula
-  url "https://github.com/balena-io/balena-cli/releases/download/v12.37.1/balena-cli-v12.37.1-macOS-x64-standalone.zip"
-  version "12.37.1"
-  sha256 "3365114846be8886f7cf4f922224a162e137c0e63787ff59873221b27bab0bdd"
+  url "https://github.com/balena-io/balena-cli/releases/download/v12.38.1/balena-cli-v12.38.1-macOS-x64-standalone.zip"
+  version "12.38.1"
+  sha256 "04427aa8e55814487e8a257623a72dd7064c1129a8cff14b6de8d27ba6228917"
 
   def install
-    share.install Dir['*']
-    bin.mkdir
-    ln_sf (share/"balena").to_s, (bin/"balena").to_s
+    libexec.install Dir['*']
+    bin.install_symlink libexec/"balena"
   end
 end
---
+++ Formula/lua-language-server.rb
@@ -0,0 +1,17 @@
+class LuaLanguageServer < Formula
+  desc "LSP for Lua"
+  homepage "https://github.com/sumneko/lua-language-server"
+  url "https://github.com/sumneko/lua-language-server.git"
+  version "HEAD"
+
+  depends_on "ninja" => :build
+
+  def install
+    cd '3rd/luamake' do
+      system 'ninja', '-f', 'ninja/macos.ninja'
+    end
+    system './3rd/luamake/luamake', 'rebuild'
+    bin.install Dir['bin/macOS/*']
+    libexec.install 'main.lua', 'platform.lua', 'script', 'locale', 'debugger.lua'
+  end
+end
